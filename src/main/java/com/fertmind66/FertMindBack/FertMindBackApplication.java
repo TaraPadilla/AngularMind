9c9a5ba123a599c535f113ae40ca658ecaa26517
TaraPadilla <daalejito20@gmail.com>
2024-10-06 17:17:35 -0500

    Initial commit with basic project setup

---
+++ src/main/java/com/fertmind66/FertMindBack/FertMindBackApplication.java
@@ -0,0 +1,13 @@
+package com.fertmind66.FertMindBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FertMindBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FertMindBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fertmind66/FertMindBack/FertMindBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fertmind66.FertMindBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FertMindBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
